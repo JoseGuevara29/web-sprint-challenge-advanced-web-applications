fe985ce8b97030ff060862a6455c7591a61f2eb5
joseguevara29 <guevarabus29@gmail.com>
2021-07-18 23:44:35 -0400

    Completed

--- interview_answers.md
+++ interview_answers.md
@@ -1,11 +1,15 @@
 # Interview Answers
+
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. These will not be counted as a part of your sprint score but will be helpful for preparing you for your endorsement interview, and enhancing overall understanding.
 
-
 1. Explain what a token is used for.
-
+   Tokens are used for authentications, they are stored in local storage and are used to let users log into a secure part of your site.
 2. What steps can you take in your web apps to keep your data secure?
-
+   using tokens, enforcing strong password, and protected routes are some ways out of many to keep any website safe.
 3. Describe how web servers work.
-
+   web servers are computers that run the code for websites.
 4. Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+   Create = POST
+   Read = GET
+   Update = PUT
+   Delete = DELETE
--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import MutationObserver from 'mutationobserver-shim';
+import React from "react";
+import MutationObserver from "mutationobserver-shim";
 
-import { render, screen} from "@testing-library/react";
-import BubblePage from './BubblePage';
+import { render, screen, wait, waitFor } from "@testing-library/react";
+import BubblePage from "./BubblePage";
+import fetchColorServices from "../services/fetchColorService";
+import userEvent from "@testing-library/user-event";
+jest.mock("../services/fetchColorService");
 
-test("Renders without errors", ()=> {
-    
+const testColor = {
+  color: "blue",
+  code: { hex: "#7fffd4" },
+  id: 1,
+};
+
+test("Renders without errors", () => {
+  render(<BubblePage />);
 });
 
-test("Renders appropriate number of colors passed in through mock", async ()=> {
-    //Keep in mind that our service is called on mount for this component.
+test("Renders appropriate number of colors passed in through mock", async () => {
+  //Keep in mind that our service is called on mount for this component.
+  fetchColorServices.mockResolvedValueOnce(testColor);
+
+  render(<BubblePage />);
+  const colors = screen.getAllByTestId("color");
+
+  await waitFor(() => {
+    expect(colors).toHaveLength(1);
+  });
 });
